01d666723389c25034d4e50d7ee8461e3b61d539
Pedro Gabriel <pedrogabrieldev@gmail.com>
2022-06-30 16:16:41 -0300

    Lift-off IV: Mutations

--- src/containers/track-card.js
+++ src/containers/track-card.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { gql, useMutation } from '@apollo/client'
 import { Link } from '@reach/router'
 import styled from '@emotion/styled'
 import { colors, mq } from '../styles'
 import { humanReadableTimeFromSeconds } from '../utils/helpers'
+
+/**
+ * Mutation to increment a track's number of views
+ * (exported for tests)
+ */
+export const INCREMENT_TRACK_VIEWS = gql`
+  mutation IncrementTrackViewsMutation($incrementTrackViewsId: ID!) {
+    incrementTrackViews(id: $incrementTrackViewsId) {
+      code
+      success
+      message
+      track {
+        id
+        numberOfViews
+      }
+    }
+  }
+`
 
 /**
  * Track Card component renders basic info in a card format
@@ -11,8 +30,16 @@ import { humanReadableTimeFromSeconds } from '../utils/helpers'
 const TrackCard = ({ track }) => {
   const { title, thumbnail, author, length, modulesCount, id } = track
 
+  const [incrementTrackViews] = useMutation(INCREMENT_TRACK_VIEWS, {
+    variables: { incrementTrackViewsId: id },
+    // to observe what the mutation response returns
+    onCompleted: (data) => {
+      console.log(data)
+    },
+  })
+
   return (
-    <CardContainer to={`/track/${id}`}>
+    <CardContainer to={`/track/${id}`} onClick={incrementTrackViews}>
       <CardContent>
         <CardImageContainer>
           <CardImage src={thumbnail} alt={title} />
