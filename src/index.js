fc6df0650b42fbc2c9f1ba5f26886312e111e950
Pedro Gabriel <pedrogabrieldev@gmail.com>
2022-06-30 23:30:45 -0300

    Lift-off V: Production & the Schema Registry

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import GlobalStyles from './styles'
 import Pages from './pages'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://catstronauts-server-w89yx8.herokuapp.com/',
   cache: new InMemoryCache(),
 })
 
